880033716a130b4ae20fa8e321d668454704bdb9
omkargunge <omkargunge01@gmail.com>
2025-12-09 22:08:36 +0530

    add2

--- c language/addsubmuldiv.c
+++ c language/addsubmuldiv.c
@@ -5,7 +5,7 @@ int main(){
     int num1,num2,add,sub,mul;
     float div;
 
-    printf("enter any two number");
+    printf("enter any two numbers");
     scanf("%d%d",&num1,&num2);
 
     add=num1+num2;
