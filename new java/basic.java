540c2a72b1cb87cbc7d3318f846f94f631c3c068
omkargunge <omkargunge01@gmail.com>
2026-01-08 12:43:01 +0530

    1

---
+++ new java/basic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class basic{
+
+    public static void main(String[]args){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter your name");
+        String name=sc.next();
+
+        System.out.println("enter your age");
+        int age=sc.nextInt();
+
+        System.out.println("Name:"+name);;
+        System.out.println(age>18?"eligibal to vote":"not eligibal to vote");
+
+    }
+}
