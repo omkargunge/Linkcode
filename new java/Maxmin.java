4956e9311fd8792ceb810e477fc6d002fabae0ae
omkargunge <omkargunge01@gmail.com>
2026-01-09 22:59:05 +0530

    3

---
+++ new java/Maxmin.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Maxmin {
+
+    public static void main(String []args){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter num 1");
+        int num1=sc.nextInt();
+
+        System.out.println("Enter num 2");
+        int num2=sc.nextInt();
+
+        
+    
+
+    }
+    
+}
