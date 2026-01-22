05df1627f76167244a05bcc8ddcec047c6f64c2f
omkargunge <omkargunge01@gmail.com>
2026-01-22 12:03:54 +0530

    1

---
+++ new java/str.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class str{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+
+        //sc.nextline();
+
+        System.out.println("enter one string");
+        String str=sc.nextLine();
+
+        String remove="";
+
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(remove.indexOf(ch)==-1){
+                remove+=ch;
+            }
+
+        }
+        System.out.println("after removing duplicate charchter " +remove);
+    }
+
+}
