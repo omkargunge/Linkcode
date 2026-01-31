43878192ca8b27ead47cb7ad7ee2a4cfa1c97e5f
omkargunge <omkargunge01@gmail.com>
2026-01-31 11:00:22 +0530

    2

---
+++ new java/typesting2.java
@@ -0,0 +1,22 @@
+public class typesting2 {
+    public static void main(String[] args) {
+        
+        char x='o';
+        
+        //narrowing
+        int a=(int)x;
+        System.out.println(a);
+        //narrowing
+        float b=(float)x;
+        System.out.println(b);
+        //narrowing
+        byte c=(byte)x;
+        System.out.println(c);
+
+
+
+    
+
+    }
+    
+}
