27b2c65da65bdbd763a3767d0fd4bb796cb1df72
omkargunge <omkargunge01@gmail.com>
2026-01-31 11:46:36 +0530

    4

---
+++ new java/ifelse3.java
@@ -0,0 +1,18 @@
+public class ifelse3 {
+    
+    
+    public static void main (String[] args){
+        int a=5;
+        int b=5;
+        if(a==b){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+}
+
+
+
+
