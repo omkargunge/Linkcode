fdb52e8080973ef396847c7fdfd9d6458a81b2f5
omkargunge <omkargunge01@gmail.com>
2026-01-31 11:36:25 +0530

    3

---
+++ new java/ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main (String[] args){
+        int age=8;
+        if(age>=18){
+            System.out.println("eligibal");
+        }
+        else{
+            System.out.println("not eligibal");
+        }
+    }
+}
