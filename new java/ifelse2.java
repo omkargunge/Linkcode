54abba851ae87b6a6d0358e06ba9aee73c1f013b
omkargunge <omkargunge01@gmail.com>
2026-01-31 11:42:09 +0530

    1

---
+++ new java/ifelse2.java
@@ -0,0 +1,14 @@
+public class ifelse2 {
+    
+    public static void main (String[] args){
+        int a=7;
+        if(a<10){
+            System.out.println("7 is less than 10");
+        }
+        else{
+            System.out.println("7 is greater than 10");
+        }
+    }
+}
+
+
