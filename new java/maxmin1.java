664b0f1aff745cf95b1b553e22a5d861c071e2d3
omkargunge <omkargunge01@gmail.com>
2026-04-22 14:45:10 +0530

    1

---
+++ new java/maxmin1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class maxmin1 {
+
+    public static void main(String[]args){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter size for an array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        System.out.println("enter value for an array");
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+
+        int max=arr[0];
+        for(int i=1;i<n;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+
+
+
+    
+}
+}
