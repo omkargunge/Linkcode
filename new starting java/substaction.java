e1bc01d03833cc0084c453aaeada18137f66cacd
omkargunge <omkargunge01@gmail.com>
2026-07-15 17:51:12 +0530

    2

---
+++ new starting java/substaction.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class substaction {
+    
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the two number");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("substraction :" + a + "-" + b + "=" + (a - b));
+
+    }
+
+}
