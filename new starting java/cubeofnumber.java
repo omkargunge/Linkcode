b70f39ea279445bf04739be25407fa2ee25fc97a
omkargunge <omkargunge01@gmail.com>
2026-07-15 18:08:46 +0530

    6

---
+++ new starting java/cubeofnumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class cubeofnumber {
+    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number:");
+        int n=sc.nextInt();
+
+        System.out.println("cube of:"+n+"="+(n*n*n));
+
+    }
+}
+
+
+
+
+
+
+
