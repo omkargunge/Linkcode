3f092d2f52283d9895dcc575afed3f4913f6816c
omkargunge <omkargunge01@gmail.com>
2026-07-15 18:04:25 +0530

    5

---
+++ new starting java/areaofcircle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class areaofcircle {
+      
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the radius:");
+        double r=sc.nextDouble();
+
+        double area=3.14*r*r;
+
+
+        System.out.println("area of circle:" +area);
+
+    }
+}
+
+
+
+
+
