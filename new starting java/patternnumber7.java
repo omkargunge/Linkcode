7835557cd59e212e1c2636ba623740e13a73e4c6
omkargunge <omkargunge01@gmail.com>
2026-07-18 18:20:44 +0530

    7

---
+++ new starting java/patternnumber7.java
@@ -0,0 +1,24 @@
+public class patternnumber7 {
+    
+    
+    
+    
+    public static void main(String[]args){
+        for (int i=1;i<=5;i++){
+            for(int j=i;j<=5;j++){
+                
+                System.out.print(j);
+            
+            }
+             System.out.println();
+        }
+    }
+    
+}
+
+
+
+
+
+
+
