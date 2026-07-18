0043449736c8a5ae167a71c1ccb2fc2a81157554
omkargunge <omkargunge01@gmail.com>
2026-07-18 12:41:00 +0530

    1

---
+++ new starting java/loopmax.java
@@ -0,0 +1,17 @@
+public class loopmax {
+    public static void main(String[]args){
+        
+        int arr[]={12,3,45,6,7,};
+        int max=arr[0];
+
+        for(int i=0;i<5;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+
+    
+
+}
