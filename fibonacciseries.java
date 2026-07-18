6b27ada5e3db8864e5ee0d55deefd72176006a3e
omkargunge <omkargunge01@gmail.com>
2026-07-18 14:07:56 +0530

    13

---
+++ fibonacciseries.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class fibonacciseries {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        sc.close();
+    }
+}
+
