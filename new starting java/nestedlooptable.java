d93207edc888eec7fd98220d879e74c3efd86d90
omkargunge <omkargunge01@gmail.com>
2026-07-18 13:08:51 +0530

    6

---
+++ new starting java/nestedlooptable.java
@@ -0,0 +1,15 @@
+public class nestedlooptable {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=20){
+            System.out.println("table of"+i);
+            int j=1;
+            while(j<=10){
+                System.out.println(i+"*"+j+"="+(i*j));
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
---
+++ new starting java/whileloop2table.java
@@ -0,0 +1,9 @@
+public class whileloop2table {
+    public static void main(String[] args) {
+        int n=2,a=1;
+        while(a<=10){
+            System.out.println(a+"*"+n+"="+(n*a));
+            a++;
+        }
+    }
+}
