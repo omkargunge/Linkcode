41fe73a1fc6b39a8378b22bccdae9a56267b9091
omkargunge <omkargunge01@gmail.com>
2026-07-18 18:24:11 +0530

    8

---
+++ new starting java/patterncharacter.java
@@ -0,0 +1,24 @@
+public class patterncharacter {
+    
+    
+    public static void main(String[]args){
+        for (char i='a';i<='e';i++){
+            for(char j='a';j<=i;j++){
+                
+                System.out.print(j);
+            
+            }
+             System.out.println();
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
