3d1f29c17cd269b87e3f834e89da6658be495207
omkargunge <omkargunge01@gmail.com>
2026-07-18 18:18:30 +0530

    6

---
+++ new starting java/patternnumber4.java
@@ -0,0 +1,20 @@
+public class patternnumber4 {
+    
+    public static void main(String[]args){
+        for (int i=1;i<=5;i++){
+            if(i%2==1){
+                for(int j=1;j<=5;j++){
+                System.out.print(1);
+            }
+        }else{
+             for(int j=1;j<=5;j++){
+                System.out.print("0");
+        }
+            
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
