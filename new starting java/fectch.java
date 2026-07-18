cc3b08169f420393b0bfe3b539f4b310f23fa436
omkargunge <omkargunge01@gmail.com>
2026-07-18 13:13:52 +0530

    7

---
+++ new starting java/fectch.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class fectch {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter one number");
+        int num=sc.nextInt();
+
+        while(num>0){
+            int digit=num%10;
+            System.out.println(digit);
+            num=num/10;
+        }
+
+    }
+}
