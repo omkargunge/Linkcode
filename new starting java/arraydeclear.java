daca3cf575e59ec48fcfd3c7e0fe61f58f9c4843
omkargunge <omkargunge01@gmail.com>
2026-07-19 16:55:07 +0530

    2

---
+++ new starting java/arraydeclear.java
@@ -0,0 +1,16 @@
+public class arraydeclear {
+    
+    public static void main(String[] args) {
+
+        // Declare and initialize an array of 5 integers
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print all elements of the array
+        System.out.println("Array elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
